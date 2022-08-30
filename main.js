103063649fc87e7b70bd0e3b6aebaba47047d54e
Nessaj44 <johnedmoncoc@gmail.com>
2022-08-30 11:28:16 +0800

    Add create grid function

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('test')
+function createGrid (num = 16) {
+  const container = document.querySelector('#container');
+  for(let i = 0; i < (num * num); i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    container.appendChild(box);
+  }
+  container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+}
+createGrid()
