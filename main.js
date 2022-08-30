f574d6d1aaa3300723baac34fbc8e0b9830967af
Nessaj44 <johnedmoncoc@gmail.com>
2022-08-30 13:29:00 +0800

    Add a button to create grid

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-function createGrid (num = 16) {
-  const container = document.querySelector('#container');
-  for(let i = 0; i < (num * num); i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    container.appendChild(box);
+function createGrid (num) {
+  const container = document.querySelector('#container')
+  num = document.querySelector('#number').value;
+  
+  if(num > 100 || num === '') {
+    alert('Pls enter 1 - 100');
+  } else {
+    for(let i = 0; i < (num * num); i++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      container.appendChild(box);
+    }
   }
+
   container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 }
-createGrid()
+
+
